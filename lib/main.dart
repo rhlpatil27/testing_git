7a2e14b8deb97b529b9639ab032dd671e5538a9a
rahulpatil <rhlpatil27@gmail.com>
2023-09-15 09:22:44 +0530

    first commit on add func

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo  testing',
+      title: 'Flutter Demo  add func',
       theme: ThemeData(
         // This is the theme of your application.
         //
