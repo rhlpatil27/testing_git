a50bde49ff37dbce999e84788bd943bc7fdfdfb0
Rahul Patil <patil.rahul@icicisecurities.com>
2024-01-19 13:13:46 +0530

    brnach protection rules checking

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo  add func bhy rahul',
+      title: 'Flutter Demo  add func bhy rahul protect',
       theme: ThemeData(
         // This is the theme of your application.
         //
